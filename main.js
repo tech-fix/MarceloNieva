d1fcf14b9df8deb5141da59e0c097991e4a8c2ac
techfix <67394973+tech-fix@users.noreply.github.com>
2020-09-03 23:42:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var menu = document.getElementById('menuu');
+var burgerMenu1 = document.getElementById('burguer1');
+var burgerMenu2 = document.getElementById('burguer2');
+var burgerMenu3 = document.getElementById('burguer3');
+var nav = document.getElementById('navbarr');
+var nombre = document.getElementById('nombree');
+var linea = document.getElementById('lineaa');
+menu.addEventListener('click', function(){
+    this.classList.toggle("abierta");
+    nav.classList.toggle("abierta");
+    burgerMenu1.classList.toggle("abierta");
+    burgerMenu2.classList.toggle("abierta");
+    burgerMenu3.classList.toggle("abierta");
+    nombre.classList.toggle("abierta");
+    lineaa.classList.toggle("abierta");
+})
